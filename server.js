b987f2829027bc9a5a14c904c0b00c203143b50f
lindlaaa <develop.lindley@gmail.com>
2018-11-02 11:48:22 -0500

    Added tests

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ var app = express();
 var bodyParser = require('body-parser')
 let {saveHistory} = require('./history')
 
-const PORT = 3001
+app.port = 3001
 
 app.use( bodyParser.json() );       // to support JSON-encoded bodies
 app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
@@ -13,7 +13,7 @@ app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
 
 app.get('/', function(req, res){
 	// TODO
-	res.send('hello History!');
+	res.send('Hello History!');
 });
 
 app.get('/health', function(req, res) {
@@ -35,5 +35,6 @@ app.post('/api', function(req, res) {
 	saveHistory(req, (result) => res.send(result))
 })
 
-console.log('Listening on port:',PORT)
-app.listen(PORT);
+app.listen(app.port, () => console.log(`Cadence listening on port ${app.port}!`))
+
+module.exports = app; // For testing.
